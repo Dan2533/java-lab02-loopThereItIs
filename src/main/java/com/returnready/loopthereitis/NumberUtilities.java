1312d16f61a500398b0fcc5e8a5e954bbeb1d822
Dan2533 <daniels1515@gmail.com>
2021-01-11 20:44:46 -0500

    Finished Project

--- src/main/java/com/returnready/loopthereitis/NumberUtilities.java
+++ src/main/java/com/returnready/loopthereitis/NumberUtilities.java
@@ -3,34 +3,80 @@ package com.returnready.loopthereitis;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+     StringBuilder resultingStringBuilder = new StringBuilder();
+     for(int i = start;i<=stop;i++){
+         if(i%2==0){
+             resultingStringBuilder.append(String.valueOf(i));
+         }
+     }
+        return resultingStringBuilder.toString();
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+      String oddNumbers = "";
+      for (int i = start; i < stop; i++) {
+          if(i % 2 != 0){
+              oddNumbers = oddNumbers + i;
+          }
+      }
+        return oddNumbers;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String squareNumbers = "";
+        for(int i= start; i < stop; i += step) {
+            squareNumbers += i *i;
+        };
+        return squareNumbers;
     }
 
     public static String getRange(int start) {
-        return null;
+        String left = "";
+        for(int i = 0; i < start; i++){
+            left = left + i;
+        }
+
+        // i = 0: a = 0
+        // i = 1: a = 01
+        // i = 2: a = 012
+        // i = 3: a = 0123
+        // i = 4: a = 01234
+        // i = 5: a = 012345
+        // i = 6: a = 0123456
+        // i = 7: a = 01234567
+        // i = 8: a = 012345678
+        // i = 9: a = 0123456789
+
+        return left;
+
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String l = "";
+        for(int i = start; i<stop;i++){
+          l = l + i;
+        }
+
+        return l;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+       String j = "";
+       for(int i = start;i<stop;i+=step){
+           j = j + i;
+       }
+        return j;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String l = "";
+        for(int i = start; i<stop;i+=step) {
+            l = l + (int)Math.pow(i,exponent);
+        }
+        return l;
     }
 }
--- src/main/java/com/returnready/loopthereitis/TableUtilities.java
+++ src/main/java/com/returnready/loopthereitis/TableUtilities.java
@@ -2,7 +2,24 @@ package com.returnready.loopthereitis;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String returnedTable= "";
+        int rowSize = 5;
+        int columnSize = 5;
+        for( int i = 1; i <= columnSize;i++){
+            int result;
+            for(int j = 1; j <= rowSize; j++){
+                result = i * j;
+                returnedTable += " ";
+                if (result <10){
+                    returnedTable +=" ";
+                }
+                returnedTable += result;
+                returnedTable += " |";
+            }
+            returnedTable += "\n";
+
+        } return returnedTable;
+
     }
 
     public static String getLargeMultiplicationTable() {
